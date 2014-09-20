b34f2c0450f89b1a0e2077f1990b3c43297c6dac
acreilly <acreilly3@gmail.com>
2014-09-20 14:45:36 -0400

    api call grabs all market names and distances

--- js/marketsearch.js
+++ js/marketsearch.js
@@ -1,19 +1,63 @@
-(function(){
-  var app = angular.module("FarmersMarkets", ['ngRoute'])
+// (function(){
+//   var app = angular.module("FarmersMarkets", ['ngRoute'])
 
-  app.controller("MarketSearchController", function(){
-    this.searchMarket = function(zipcode){
-      var zip = parseInt(zipcode)
+//   app.controller("MarketSearchController", function(){
+    // this.searchMarket = function(zipcode){
+    //   var zip = parseInt(zipcode)
+    //   $.ajax({
+    //     type: "GET",
+    //     contentType: "application/json; charset=utf-8",
+    //     url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zip,
+    //     dataType: 'jsonp',
+    //     jsonpCallback: 'searchResultsHandler'
+    //   }).done(function(){
+    //     debugger
+    //   });
+    // }
+  // });
+
+// });
+
+$(document).ready(function(){
+var search = new MarketSearchController()
+$("form").on("submit", function(){
+  search.searchMarket()
+})
+$("ul").on("click", "li", function(){
+  search.getDetails()
+})
+})
+
+function MarketSearchController(){}
+MarketSearchController.prototype = {
+  searchMarket: function(){
+    event.preventDefault()
+      var zip = $("input").val()
       $.ajax({
         type: "GET",
         contentType: "application/json; charset=utf-8",
         url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zip,
         dataType: 'jsonp',
         jsonpCallback: 'searchResultsHandler'
-      }).done(function(){
-        debugger
+      }).done(function(data){
+        $(data.results).each(function(){
+          var market = this.marketname
+          var marketname = market.substr(market.indexOf(' ')+1);
+          var miles = market.substr(0,market.indexOf(' '));
+        $(".markets").append("<li id='" + this.id + "'>" + miles + " miles: " + marketname + "</li>")
+        })
       });
-    }
-  });
-
-});
+  },
+  getDetails: function(){
+    var marketID = this.id;
+debugger
+    $.ajax({
+        type: "GET",
+        contentType: "application/json; charset=utf-8",
+        url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + marketID,
+        dataType: 'jsonp',
+        jsonpCallback: 'detailResultHandler'
+    }).done(function(data){
+    });
+  }
+}
