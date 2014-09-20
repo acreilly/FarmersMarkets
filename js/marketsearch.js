a1e6eab67788b54eb55cc08b5f839922bd0ea214
acreilly <acreilly3@gmail.com>
2014-09-20 13:07:44 -0400

    first commit

---
+++ js/marketsearch.js
@@ -0,0 +1,19 @@
+(function(){
+  var app = angular.module("FarmersMarkets", [])
+
+  app.controller("MarketSearchController", function(){
+    this.searchMarket = function(zipcode){
+      var zip = parseInt(zipcode)
+      $.ajax({
+        type: "GET",
+        contentType: "application/json; charset=utf-8",
+        url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zip,
+        dataType: 'jsonp',
+        jsonpCallback: 'searchResultsHandler'
+      }).done(function(){
+        debugger
+      });
+    }
+  });
+
+})
