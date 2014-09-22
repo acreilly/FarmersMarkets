2c3e7c59292d4b89402d5e6895dcec256676767a
acreilly <acreilly3@gmail.com>
2014-09-22 10:17:03 -0400

    organized and cleaned up javascript code. made it more readable. made footer static instead of absolute.

--- js/marketsearch.js
+++ js/marketsearch.js
@@ -1,44 +1,26 @@
-// (function(){
-//   var app = angular.module("FarmersMarkets", ['ngRoute'])
-
-//   app.controller("MarketSearchController", function(){
-    // this.searchMarket = function(zipcode){
-    //   var zip = parseInt(zipcode)
-    //   $.ajax({
-    //     type: "GET",
-    //     contentType: "application/json; charset=utf-8",
-    //     url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zip,
-    //     dataType: 'jsonp',
-    //     jsonpCallback: 'searchResultsHandler'
-    //   }).done(function(){
-    //     debugger
-    //   });
-    // }
-  // });
-
-// });
-
 $(document).ready(function(){
   var view = new MarketSearchView()
   var search = new MarketSearchController(view)
-  $("form").on("submit", function(){
-    search.view.removeMarkets()
-    search.searchMarket()
-  })
-  $("ul").on("click", "span", function(){
-    search.getDetails(this)
-  })
+  search.initialize()
 })
 
 
 function MarketSearchView(){}
 MarketSearchView.prototype = {
-  addMarkets: function(){
+  addMarkets: function(data){
+    $(data).each(function(){
+      var market = this.marketname,
+      marketname = market.substr(market.indexOf(' ')+1),
+      miles = market.substr(0,market.indexOf(' '));
 
+      $(".markets").append(
+        "<li><span id='" + this.id + "'>"+ miles +
+        " miles: " + marketname + "</span></li>"
+        )
+    })
   },
   removeMarkets: function(){
     $(".container").addClass("searchBox")
-
   },
   addOrRemoveDetails: function(market, details){
     var address = details.Address,
@@ -49,10 +31,19 @@ MarketSearchView.prototype = {
       products = details.Products,
       schedule = details.Schedule,
       api_key = 'AIzaSyCyRtY733lg5p6a1ZOJeY8hS3xbBXhEfOE';
+
       if($(market).siblings().length === 0){
-        $(market).after("<ul class='info'><li><strong> Schedule: </strong>" + schedule + "</li><li><strong>Address: </strong>" + address + "</li><li><strong>Products: </strong>" + products + "</li><li><iframe src='https://www.google.com/maps/embed/v1/place?key=" + api_key + "&" + googleCenter + "'></iframe></li></ul>")
+
+        $(market).after(
+          "<ul class='info'><li><strong> Schedule: </strong>" +
+          schedule + "</li><li><strong>Address: </strong>" +
+          address + "</li><li><strong>Products: </strong>" +
+          products + "</li><li><iframe src='https://www.google.com/maps/embed/v1/place?key=" +
+          api_key + "&" + googleCenter + "'></iframe></li></ul>"
+          )
+
         $(market).addClass("active")
-      } else{
+      } else {
         this.removeDetails(market);
       }
     },
@@ -66,6 +57,16 @@ MarketSearchView.prototype = {
     this.view = view;
   }
   MarketSearchController.prototype = {
+    initialize: function(){
+      var self = this;
+      $("form").on("submit", function(){
+        self.view.removeMarkets()
+        self.searchMarket()
+      })
+      $("ul").on("click", "span", function(){
+        self.getDetails(this)
+      })
+    },
     searchMarket: function(){
       event.preventDefault()
       $(".markets")[0].innerHTML = ""
@@ -77,18 +78,17 @@ MarketSearchView.prototype = {
         dataType: 'jsonp',
         jsonpCallback: 'searchResultsHandler'
       }).done(function(data){
-        if(data.results[0].id === "Error"){
-          $(".markets").append("<li><strong>" + data.results[0].marketname + "</strong></li>");
-        } else {
-          $(data.results).each(function(){
-            var market = this.marketname
-            var marketname = market.substr(market.indexOf(' ')+1);
-            var miles = market.substr(0,market.indexOf(' '));
-            $(".markets").append("<li><span id='" + this.id + "'>" + miles + " miles: " + marketname + "</span></li>")
 
-          })
+        if(data.results[0].id === "Error"){
+
+          $(".markets").append(
+            "<li><strong>" + data.results[0].marketname + "</strong></li>"
+            );
+
+        } else {
+          this.view.addMarkets(data.results)
         }
-      });
+      }.bind(this));
     },
     getDetails: function(market){
       this.market = market;
@@ -100,7 +100,7 @@ MarketSearchView.prototype = {
         jsonpCallback: 'detailResultHandler'
       }).done(function(data){
         var details = data.marketdetails;
-        this.view.addOrRemoveDetails(this.market, details)
+        this.view.addOrRemoveDetails(this.market, details);
       }.bind(this));
     }
   }
